5882f0b0169bd2732c87e04fd8552042bdf29ca1
Sharizan Redzuan <ijanzz5621@gmail.com>
2017-12-15 15:27:21 +0800

    initial

---
+++ server/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+//public route
+app.get('/api/deals/public', (req, res) => {
+    let deals = [
+        // Array of public deals here
+    ];
+
+    res.json(deals);
+});
+
+//private route
+app.get('/api/deals/private', (req, res) => {
+    let deals = [
+        // Array of private deals here
+    ];
+
+    res.json(deals);
+});
+
+app.listen(3001);
+console.log('Serving deals on localhost:3001');
